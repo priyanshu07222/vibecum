c555fe38fdd0f5da0b5ec31e91db5f158c24067b
Priyanshu Tiwari <priyanshu16181389@gmail.com>
2025-06-25 23:57:05 +0530

    create msg db

--- db/migrations/2025-06-20-114843_create_user/up.sql
+++ db/migrations/2025-06-20-114843_create_user/up.sql
@@ -1,7 +1,8 @@
 -- Your SQL goes here
 CREATE table users (
-    id TEXT NOT NULL PRIMARY KEY,
-    user_name VARCHAR NOT NULL,
-    password VARCHAR NOT NULL,  
-    name TEXT
+    id          TEXT NOT NULL PRIMARY KEY,
+    user_name   VARCHAR(50) NOT NULL,
+    password    VARCHAR(50) NOT NULL,  
+    name        TEXT,
+    create_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
--- db/migrations/2025-06-23-180621_create_room/up.sql
+++ db/migrations/2025-06-23-180621_create_room/up.sql
@@ -1,5 +1,27 @@
 -- Your SQL goes here
 CREATE TABLE room (
- id  TEXT NOT NULL PRIMARY,
- codee VARCHAR NOT NULL UNIQUE
+ id            VARCHAR(100) NOT NULL UNIQUE PRIMARY,
+ code          CHAR(9) UNIQUE NOT NULL,
+ name          VARCHAR(50),
+ admin_id      VARCHAR(100)
+ created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+ CONSTRAINT admin FOREIGN KEY (adminId) REFERENCES users(id)
+);
+
+
+CREATE TABLE room_user(
+id          VARCHAR(100) NOT NULL UNIQUE PRIMARY,
+user_id     VARCHAR(100) NOT NULL,
+room_id     VARCHAR(100) NOT NULL,
+role_assign role DEFAULT "USER"
+CONSTRAINT user FOREIGN KEY (user_id) REFERENCES users(id),
+CONSTRAINT user FOREIGN KEY (room_id) REFERENCES room(id)
+);
+
+CREATE TABLE message(
+id             VARCHAR(100) PRIMARY,
+
 )
+
+
+CREATE TYPE role AS ENUM ("USER" , "ADMIN");
